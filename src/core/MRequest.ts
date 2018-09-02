a70c00836cb1da169fc63c8b58472c655b1bb4af
bennnis <305945915@qq.com>
2018-09-02 19:11:03 +0800

    complete request posts

--- src/core/MRequest.ts
+++ src/core/MRequest.ts
@@ -6,26 +6,35 @@ import findMatchStrategy from "../../utils/findMatchStrategy";
 import {GET_FLAG} from "../config/requestMethods";
 
 export default class MRequest {
+    // `_uuid`
+    // "Universally Unique Identifier" for marking per request:
     private _uuid: Number = ~~(Math.random() * 10e8)
-    // `url` is the server URL that will be used for the request
+
+    // `url`
+    // Server URL that will be used for the request
     url: String
 
-    // `method` is the request method to be used when making the request
+    // `method`
+    // request method to be used when making the request
     method: String
 
-    // `baseURL` will be prepended to `url` unless `url` is absolute.
-    // It can be convenient to set `baseURL` for an instance of axios to pass relative URLs
+    // `baseURL`
+    // will be prepended to `url` unless `url` is absolute such as http://* or https://*.
+    // It can be convenient to set `baseURL` for an instance of majax to pass relative URLs
     // to methods of that instance.
     baseURL: String
 
-    // `headers` are custom headers to be sent
+    // `headers`
+    // custom headers to be sent
     headers: Object
 
-    // `params` are the URL parameters to be sent with the request
+    // `params`
+    // URL parameters to be sent with the request
     // Must be a plain object or a URLSearchParams object
     params: Object
 
-    // `data` is the data to be sent as the request body
+    // `data`
+    // data to be sent as the request body
     // Only applicable for request methods 'PUT', 'POST', and 'PATCH'
     // When no `transformRequest` is set, must be of one of the following types:
     // - string, plain object, ArrayBuffer, ArrayBufferView, URLSearchParams
@@ -33,34 +42,43 @@ export default class MRequest {
     // - Node only: Stream, Buffer
     data: Object
 
-    // `timeout` specifies the number of milliseconds before the request times out.
+    // `timeout`
+    // specifies the number of milliseconds before the request times out.
     // If the request takes longer than `timeout`, the request will be aborted.
     timeout: Number
 
-    // `withCredentials` indicates whether or not cross-site Access-Control requests
+    // `withCredentials`
+    // indicates whether or not cross-site Access-Control requests
     // should be made using credentials
     withCredentials: Boolean
 
-    // `responseType` indicates the type of data that the server will respond with
+    // `responseType`
+    // indicates the type of data that the server will respond with
     // options are 'arraybuffer', 'blob', 'document', 'json', 'text', 'stream'
     responseType: String // default
 
-    // `_xhr` is ajax request driver
+    // `_xhr`
+    // XMLHttpRequest for sending real ajax request
     xhr: XMLHttpRequest
 
-    // `config` is what pass through the whole request flow
-    config: any
+    // `config`
+    // is what pass through the whole request flow
+    config: Object
 
-    // `withRushStore` will restore cache data if necessary
+    // `withRushStore`
+    // will restore cache data if necessary
     withRushStore: Boolean
 
-    // `_majaxInstance` driver of this request, inject by visit
+    // `_majaxInstance`
+    // driver of this request, inject by visit
     _majaxInstance: Majax
 
     // `_onFulfilled`
+    // callback with request success
     _onFulfilled: Function
 
     // `_onFailed`
+    // callback with request failed
     _onFailed: Function
 
     constructor (
@@ -83,6 +101,9 @@ export default class MRequest {
         this._onFailed = onFailed
     }
 
+    /**
+     * @desc XMLHttpRequest initial
+     * */
     private initXHR() {
         // xhr in browser
         const xhr = createXHR(this)
@@ -103,24 +124,40 @@ export default class MRequest {
         return xhr
     }
 
+    /**
+     * @desc emit success handler running
+     * @param responseInstance
+     * */
     public success(responseInstance) {
         this._onFulfilled(responseInstance)
     }
 
+    /**
+     * @desc emit failed handler running
+     * @param responseInstance
+     * */
     public failed(responseInstance) {
         this._onFailed(responseInstance)
     }
 
+    /**
+     * @desc got uuid of request instance
+     * @return _uuid
+     * */
     public getUUID () {
         return this._uuid
     }
 
-    public accept (majaxInstance) {
+    /**
+     * @desc accept majax instance for visiting
+     * @param majaxInstance
+     * */
+    public accept (majaxInstance: Majax) {
         this._majaxInstance = majaxInstance
     }
 
     /**
-     * @description
+     * @desc prepare to send an ajax request with blow:
      * 1. check if has store strategy           | do request if not
      * 2. check if match store strategy         | do request if not
      * 3. check if has cache and not expire     | do request if not
@@ -139,6 +176,9 @@ export default class MRequest {
         }
     }
 
+    /**
+     * @desc ajax real action
+     * */
     public sendAjax() {
         this.initXHR()
         this.xhr.send(JSON.stringify(this.data))

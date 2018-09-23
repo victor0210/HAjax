122c4f81e944b95fddc2734ae0151e0d6f769dee
bennnis <305945915@qq.com>
2018-09-23 14:33:51 +0800

    transferResponseData test

--- src/utils/transferResponseData.ts
+++ src/utils/transferResponseData.ts
@@ -1,11 +1,15 @@
 import {RESPONSE_TYPE_TEXT} from "../config/responseTypes";
+import {matchType} from "./matcher";
+import {TYPE_OBJECT} from "../config/baseType";
 
 const transferResponseData = (xhr) => {
     switch (xhr.responseType) {
         case RESPONSE_TYPE_TEXT:
-            return JSON.parse(xhr.response)
-        default:
             return xhr.response
+        default:
+            return matchType(xhr.response, TYPE_OBJECT)
+                ? xhr.response
+                : JSON.parse(xhr.response)
     }
 }
 
---
+++ test/utils/thansferResponseData.spec.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef,no-tabs,no-mixed-spaces-and-tabs */
+import transferResponseData from '../../src/utils/transferResponseData'
+
+describe('utils::transferResponseData', () => {
+  it('number-string should be converted to number with responseType: json', () => {
+    const xhr1 = {
+      responseType: 'json',
+      response: '123'
+    }
+
+    expect(
+      transferResponseData(xhr1)
+    ).toEqual(123)
+  })
+
+  it('number-string shouldn`t be converted to number with responseType: text', () => {
+    const xhr1 = {
+      responseType: 'text',
+      response: '123'
+    }
+
+    expect(
+      transferResponseData(xhr1)
+    ).toEqual('123')
+  })
+
+  it('content of JSON.stringify should be converted to json object', () => {
+    const xhr = {
+      responseType: 'json',
+      response: JSON.stringify({ name: 'mx' })
+    }
+
+    expect(
+      transferResponseData(xhr)
+    ).toEqual({ name: 'mx' })
+  })
+
+  it('content of json shouldn`t be converted to another type', () => {
+    const xhr = {
+      responseType: 'json',
+      response: { name: 'mx' }
+    }
+
+    expect(
+      transferResponseData(xhr)
+    ).toEqual({ name: 'mx' })
+  })
+})

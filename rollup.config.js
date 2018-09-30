9690a29a54e83fafada8bc86ff5d5ce269ee4548
bennnis <305945915@qq.com>
2018-09-30 22:15:46 +0800

    fix prod rollup conifg

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,69 @@ import babel from 'rollup-plugin-babel'
 import typescript from 'rollup-plugin-typescript'
 import alias from 'rollup-plugin-alias'
 import resolve from 'rollup-plugin-node-resolve'
+import { uglify } from 'rollup-plugin-uglify'
 import aliasConfig from './build/alias'
 
-export default {
+const env = process.env.NODE_ENV
+
+let plugins = [
+  babel(),
+  typescript(),
+  alias({
+    resolve: ['.ts', '.js'],
+    aliasConfig
+  }),
+  resolve({
+    browser: true
+  })
+]
+
+let moduleConfig = {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
-  },
-  plugins: [
-    babel(),
-    typescript(),
-    alias({
-      resolve: ['.ts', '.js'],
-      aliasConfig
-    }),
-    resolve({
-      browser: true
-    })
-  ]
+  output: [
+    {
+      format: 'umd',
+      file: 'dist/modules/hx.umd.js',
+      name: 'umd'
+    },
+    {
+      format: 'amd',
+      file: 'dist/modules/hx.amd.js',
+      name: 'amd'
+    },
+    {
+      format: 'es',
+      file: 'dist/modules/hx.es.js',
+      name: 'es'
+    },
+    {
+      format: 'cjs',
+      file: 'dist/modules/hx.cjs.js',
+      name: 'cjs'
+    }
+  ],
+  plugins: [...plugins]
 }
+
+let injectWindowConfig = {
+  input: 'src/script.ts',
+  output: {
+    file: 'dist/hx.min.js',
+    format: 'iife'
+  },
+  moduleName: 'min',
+  plugins: [...plugins]
+}
+
+const config = [
+  moduleConfig,
+  injectWindowConfig
+]
+
+if (env === 'production') {
+  injectWindowConfig.plugins.push(
+    uglify()
+  )
+}
+
+export default config
